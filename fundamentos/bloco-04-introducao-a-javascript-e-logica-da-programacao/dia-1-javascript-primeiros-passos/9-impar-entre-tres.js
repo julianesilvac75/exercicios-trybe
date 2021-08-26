c1b195cb3ae7068f6a3ddf668c19c86cf5b169f5
Juliane Cardoso <julianesilvac75@gmail.com>
2021-08-26 13:55:12 -0300

    programa 9 concluido

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-1-javascript-primeiros-passos/9-impar-entre-tres.js
@@ -0,0 +1,9 @@
+const numero1 = 10;
+const numero2 = 8;
+const numero3 = 6;
+
+if(numero1 % 2 != 0 || numero2 % 2 != 0 || numero3 % 2 != 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
