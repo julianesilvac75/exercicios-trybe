b358d51f9f1345a856c100add0b5af8ecca6bc8c
Juliane Cardoso <julianesilvac75@gmail.com>
2021-08-26 13:53:08 -0300

    programa 8 concluido

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-1-javascript-primeiros-passos/8-par-entre-tres.js
@@ -0,0 +1,9 @@
+const numero1 = 7;
+const numero2 = 7;
+const numero3 = 9;
+
+if(numero1 % 2 === 0 || numero2 % 2 === 0 || numero3 % 2 === 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
