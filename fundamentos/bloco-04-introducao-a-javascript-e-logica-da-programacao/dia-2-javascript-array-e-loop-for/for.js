ba005c5de16bb6f0e2cf5aa4c6a9c55e8a61cdb8
Juliane Cardoso <julianesilvac75@gmail.com>
2021-08-26 18:52:38 -0300

    exercicio de fixacao - for/of

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-2-javascript-array-e-loop-for/for.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-2-javascript-array-e-loop-for/for.js
@@ -11,8 +11,16 @@
 
 //Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
 
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
-for(let index = 0; index < groceryList.length; index += 1) {
-    console.log(groceryList[index]);
+// for(let index = 0; index < groceryList.length; index += 1) {
+//     console.log(groceryList[index]);
+// }
+
+//Utilize o for/of para imprimir os elementos da lista names com o console.log() :
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let name of names) {
+    console.log(name);
 }
