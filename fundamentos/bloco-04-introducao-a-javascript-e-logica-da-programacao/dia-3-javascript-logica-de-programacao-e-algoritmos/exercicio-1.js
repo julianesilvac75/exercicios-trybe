6f70e6ea3fa2cd3644d6ac167c21b5df4ebd35f3
Juliane Cardoso <julianesilvac75@gmail.com>
2021-08-27 18:34:42 -0300

    corrige exercicio 1

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-1.js
@@ -1,12 +1,17 @@
 //Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
 
-let n = 8;
 
-for (let i = 1; i <= n; i += 1) {
-    let line = "";
-    for (let iLine = 1; iLine <= n; iLine +=1) {
-        line += "*"
+let n = 2;
+
+if(n > 1) {
+    for (let i = 1; i <= n; i += 1) {
+        let line = "";
+        for (let iLine = 1; iLine <= n; iLine +=1) {
+            line += "*"
+        }
+    
+        console.log(line);
     }
-
-    console.log(line);
+} else {
+    console.log("Insira um número maior que 1");
 }
