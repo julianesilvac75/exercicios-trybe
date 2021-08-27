e61dc412d43dfeb784e5aa39d47f55e980adc66b
Juliane Cardoso <julianesilvac75@gmail.com>
2021-08-27 18:38:59 -0300

    exercicio 2 concluido

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-2.js
@@ -0,0 +1,9 @@
+// Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. 
+
+let n = 7;
+let line = "*";
+
+for (let i = 1; i <= n; i += 1) {
+    console.log(line);
+    line += "*";
+}
