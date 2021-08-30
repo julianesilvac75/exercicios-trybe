6c8f334d90c8a1b041f030314b32083c4e33e569
Juliane Cardoso <julianesilvac75@gmail.com>
2021-08-30 20:20:07 -0300

    exercicios 1 a 4 concluidos

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-funcoes.js
@@ -0,0 +1,71 @@
+// 1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
+function palindromeChecker(string) {
+    let split = string.split("");
+    let reverse = split.reverse();
+    let join = reverse.join("");
+
+    return string === join;
+}
+
+console.log(palindromeChecker('arara') );
+
+
+console.log("----------------");
+
+//2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+function biggestNumberChecker(array) {
+    let biggestNumber = array[0];
+    for (let index = 0; index < array.length; index +=1) {
+        if (array[index] > biggestNumber) {
+            biggestNumber = array[index];
+        }
+    }
+
+    return array.indexOf(biggestNumber)
+}
+
+let test1 = [40, 7, 75, 9, 132, 427]
+
+console.log(biggestNumberChecker(test1));
+
+console.log("----------------");
+
+//3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+function lowerNumberChecker(array) {
+    let lowerNumber = array[0];
+    for (let index = 0; index < array.length; index +=1) {
+        if (array[index] < lowerNumber) {
+            lowerNumber = array[index];
+        }
+    }
+
+    return array.indexOf(lowerNumber)
+}
+
+let test2 = [2, 4, 6, 7, 10, 0, -3];
+
+console.log(lowerNumberChecker(test2));
+
+console.log("----------------");
+
+//4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+function biggerNameChecker(array) {
+    let biggerName = array[0];
+    for (let index = 0; index < array.length; index +=1) {
+        if (array[index].length > biggerName.length) {
+            biggerName = array[index];
+        }
+    }
+
+    return biggerName
+}
+
+let test3 = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];;
+
+console.log(biggerNameChecker(test3));
+
+console.log("----------------");
