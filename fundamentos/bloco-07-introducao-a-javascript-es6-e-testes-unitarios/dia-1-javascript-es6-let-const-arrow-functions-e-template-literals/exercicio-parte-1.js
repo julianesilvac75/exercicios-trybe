1a10b565052a5cc0b5916caacb7213a1ee650698
Juliane Cardoso <julianesilvac75@gmail.com>
2021-09-23 18:38:29 -0300

    parte 1 exercicio 2

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-parte-1.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-parte-1.js
@@ -1,3 +1,5 @@
+// Exercício 1
+
 const testingScope = (escopo) => {
     if (escopo === true) {
       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -8,6 +10,23 @@ const testingScope = (escopo) => {
       console.log(elseScope);
     }
     // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-  }
+}
 
-  testingScope(true);
+testingScope(true);
+
+console.log('----------------');
+
+// Exercício 2
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortArray = (array) => {
+    const sortedArray = array.sort((a, b) => a - b); // Créditos: https://medium.com/coding-at-dawn/how-to-sort-an-array-numerically-in-javascript-2b22710e3958
+    for (let index = 0; index < sortedArray.length; index += 1) {
+        array[index] = sortedArray[index];
+    }
+
+    return array.toString();
+}
+
+console.log(`Os números ${sortArray(oddsAndEvens)} se encontram ordenados de forma crescente!`);
