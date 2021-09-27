e5398160463e2c63cf5209339e26b3fe05012306
Juliane Cardoso <julianesilvac75@gmail.com>
2021-09-27 18:36:35 -0300

    parte 2, exercicio 2 concluido

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/exercicios-parte2.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/exercicios-parte2.js
@@ -5,21 +5,31 @@ const lesson1 = {
     turno: 'manhã',
   };
   
-  const lesson2 = {
-    materia: 'História',
-    numeroEstudantes: 20,
-    professor: 'Carlos',
-  };
+const lesson2 = {
+materia: 'História',
+numeroEstudantes: 20,
+professor: 'Carlos',
+};
   
-  const lesson3 = {
-    materia: 'Matemática',
-    numeroEstudantes: 10,
-    professor: 'Maria Clara',
-    turno: 'noite',
-  };
+const lesson3 = {
+materia: 'Matemática',
+numeroEstudantes: 10,
+professor: 'Maria Clara',
+turno: 'noite',
+};
+
+// Exercício 1
 
 const addShift = (obj, key, value) => obj[key] = value;
 
 addShift(lesson2, 'turno', 'noite');
 
 console.log(lesson2);
+
+console.log('----------');
+
+// Exercício 2
+
+const keyList = (obj) => Object.keys(obj);
+
+console.log(keyList(lesson1));
