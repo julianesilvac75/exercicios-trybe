cf062537493981a48ac1427356171b450a370d38
Juliane Cardoso <julianesilvac75@gmail.com>
2021-10-07 18:36:09 -0300

    Exercicio 1 concluido

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -0,0 +1,22 @@
+// 1 - Dada uma matriz, transforme em um array.
+
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+];
+
+const teste = (arrays) => {
+    return arrays[0];
+}
+  
+function flatten() {
+   return arrays.reduce((acc, currentValue) => {
+       currentValue.forEach(value => {
+           acc.push(value)
+       })
+       return acc;
+    }, [])
+}
+
+console.log(flatten());
