45017cb5f37fe0f24e8c5f3a7483a41e36ea70b4
Juliane Cardoso <julianesilvac75@gmail.com>
2021-10-09 14:34:09 -0300

    Exercicio 6 concluido

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercises.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercises.js
@@ -72,11 +72,20 @@ function techList(array, name) {
   return (array.length > 0) ? array.map(techItem) : 'Vazio!';
 }
 
+function hydrate(string) {
+  const numbersOfString = string.match(/\d+/g);
+
+  const sum = numbersOfString.reduce((acc, number) => acc + parseInt(number, 10), 0)
+
+  return (sum === 1) ? `${sum} copo de água` : `${sum} copos de água`;
+}
+
 module.exports = {
     sum,
     myRemove,
     myFizzBuzz,
     encode,
     decode,
-    techList
+    techList,
+    hydrate
 }
--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercises.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercises.test.js
@@ -4,7 +4,8 @@ const {
     myFizzBuzz,
     encode,
     decode,
-    techList
+    techList,
+    hydrate
 } = require('./exercises.js');
 
 describe('Exercício 1', () => {
@@ -113,5 +114,21 @@ describe('Exercício 5', () => {
   });
   it('Lista com 0 tecnologias deve retornar uma mensagem de erro "Vazio!"', () => {
     expect(techList([], 'Lucas')).toBe('Vazio!');
+  });
+});
+
+describe('Exercício 6', () => {
+  it('Testa se a função hydrate é definida', () => {
+    expect(hydrate).toBeDefined();
+  });
+  it('Testa se hydrate é uma função', () => {
+    expect(typeof hydrate).toBe('function');
+  });
+  it('Ao receber uma string retorne a sugestão de quantos copos de água deve-se beber', () => {
+    expect(hydrate('1 cerveja')).toBe('1 copo de água');
+    expect(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho')).toBe('7 copos de água');
+    expect(hydrate('2 shots de tequila, 2 cervejas e 1 corote')).toBe('5 copos de água');
+    expect(hydrate('1 copo de catuaba, 1 cervejas e 1 copo de vinho')).toBe('3 copos de água');
+    expect(hydrate('4 caipirinhas e 2 cervejas')).toBe('6 copos de água');
   });
 });
