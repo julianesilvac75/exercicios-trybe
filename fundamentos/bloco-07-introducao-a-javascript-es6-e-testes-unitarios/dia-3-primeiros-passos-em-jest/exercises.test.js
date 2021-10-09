55aa6ee8aae0dfad33c2025afdeccedd0f5dc3ba
Juliane Cardoso <julianesilvac75@gmail.com>
2021-10-09 11:46:50 -0300

    Exercicio 2 concluido

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercises.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercises.test.js
@@ -17,3 +17,11 @@ describe('Exercício 1', () => {
         expect(() => sum(4, '5')).toThrowError(new Error('parameters must be numbers'))
     })
 })
+
+describe('Exercício 2', () => {
+    it('Verifica se a função retorna o array esperado', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+        expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4])
+        expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4])
+    })
+})
