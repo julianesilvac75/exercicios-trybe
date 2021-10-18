77c5459bf42b1777bc5ac322dd90fc9c190476b8
Juliane Cardoso <julianesilvac75@gmail.com>
2021-10-18 20:46:16 -0300

    Exercícios de fixacao

---
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-async-await/fetch.js
@@ -0,0 +1,43 @@
+// const fetch = require('node-fetch');
+
+// const fetchJoke = () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value))
+//     .catch((error) => console.log(`Algo deu errado :( \n${error})`));
+// }
+
+// fetchJoke();
+
+// const fetch = require('node-fetch');
+
+// const fetchJoke = () => {
+//   const url = 'api.chucknorris.io/jokes/random?category=dev';
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value))
+//     .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+// }
+
+// fetchJoke();
+
+// Algo deu errado :( 
+// TypeError: Only absolute URLs are supported
+
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+}
+
+fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
