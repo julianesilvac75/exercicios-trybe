f677e2763ba7e3fe141ac537700a7323ca331f40
Juliane Cardoso <julianesilvac75@gmail.com>
2021-10-19 10:56:07 -0300

    Exercicio bonus concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus.js
@@ -5,20 +5,6 @@ const mage = {
     damage: undefined,
 };
 
-const mageFunction = () => {
-    if (mage.mana <= 15) {
-        return {
-            damage: 'Não possui mana suficiente',
-            mana: 0,
-        }
-    }
-
-    return {
-        damage: Math.round(Math.random() * ((mage.intelligence * 2) - mage.intelligence) + mage.intelligence),
-        mana: 15,
-    }
-}
-  
 const warrior = {
     healthPoints: 200,
     strength: 30,
@@ -26,20 +12,37 @@ const warrior = {
     damage: undefined
 };
 
-const warriorDamage = () => Math.round(Math.random() * ((warrior.strength * warrior.weaponDmg) - warrior.strength) + warrior.strength);
-  
 const dragon = {
-    healthPoints: 350,
-    strength: 50,
-    damage: undefined
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined
 };
 
+// 3 - Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+const mageFunction = () => {
+    if (mage.mana < 15) {
+        return {
+            damage: 'Não possui mana suficiente',
+            spentMana: 0,
+        }
+    }
+
+    return {
+        damage: Math.round(Math.random() * ((mage.intelligence * 2) - mage.intelligence) + mage.intelligence),
+        spentMana: 15,
+    }
+}
+
+//2 - Crie uma função que retorna o dano causado pelo warrior .
+const warriorDamage = () => Math.round(Math.random() * ((warrior.strength * warrior.weaponDmg) - warrior.strength) + warrior.strength);
+
+// 1 - Crie uma função que retorna o dano do dragão.
 const dragonDamage = () => Math.round(Math.random() * (dragon.strength - 15) + 15);
   
 const battleMembers = { mage, warrior, dragon };
 
+// PARTE II
 const gameActions = {
-    // 1ª HOF
     warrior: (warriorDamage) => {
         const warriorInfo = warriorDamage();
         battleMembers.dragon.healthPoints -= warriorInfo;
@@ -47,19 +50,21 @@ const gameActions = {
     },
     mage: (mageFunction) => {
         const mageInfo = mageFunction();
-        dragon.healthPoints -= mageInfo.damage;
-        mage.damage = mageInfo.damage;
-        mage.mana -= mageInfo.mana;
+        battleMembers.dragon.healthPoints -= mageInfo.damage;
+        battleMembers.mage.damage = mageInfo.damage;
+        battleMembers.mage.mana -= mageInfo.spentMana;
     },
     dragon: (dragonDamage) => {
         const dragonInfo = dragonDamage();
-        mage.healthPoints -= dragonInfo;
-        warrior.healthPoints -= dragonInfo;
-        dragon.damage = dragonInfo;
+        battleMembers.mage.healthPoints -= dragonInfo;
+        battleMembers.warrior.healthPoints -= dragonInfo;
+        battleMembers.dragon.damage = dragonInfo;
     },
     result: () => {
+        gameActions.warrior(warriorDamage);
+        gameActions.mage(mageFunction);
+        gameActions.dragon(dragonDamage);
+
         return battleMembers;
     }
   };
-
-console.log(gameActions.result());
