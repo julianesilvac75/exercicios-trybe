24333ac4c5db39fc6d5a746bec7f8d84528fdf55
Juliane Cardoso <julianesilvac75@gmail.com>
2021-10-20 10:50:36 -0300

    feat: finaliza o exercicio 2

---
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio2.js
@@ -0,0 +1,34 @@
+// 2 - Agora, dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
+
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+
+// Resposta: a ordem será A (mars), C (jupiter) e B (venus): A é apenas a chamada da função planetDistanceFromSum, que é síncrona, então aparecerá primeiro; B e C são funções assíncronas dentro das quais a função planetDistanceFromSun é chamada, porém C tem um tempo menor de execução que B, por isso aparece antes.
