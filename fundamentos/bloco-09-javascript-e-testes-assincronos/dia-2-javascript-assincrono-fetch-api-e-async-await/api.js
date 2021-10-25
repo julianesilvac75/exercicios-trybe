58018e75a5f560dfe8b344b08a7e9639a7fffa67
Juliane Cardoso <julianesilvac75@gmail.com>
2021-10-25 16:59:43 -0300

    feat: exercicio bonus concluido

--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-async-await/api.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-async-await/api.js
@@ -12,14 +12,21 @@ function fetchApi() {
       .then(data => data.data);
 }
 
+function convertToBRL(priceUsd) {
+    return fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json')
+      .then(response => response.json())
+      .then(data => priceUsd * Number(data.usd.brl));
+}
+
 async function addCoinsToDOM() {
     const coinsList = await fetchApi();
     const filtered = coinsList.filter(coin => Number(coin.rank) <= 10);
 
-    filtered.forEach(coin => {
+    filtered.forEach(async coin =>  {
         const { name, symbol, priceUsd } = coin;
+        const convertedBRL = await convertToBRL(Number(priceUsd));
         const newLi = document.createElement('li');
-        newLi.innerText = `${name} (${symbol}): ${Number(priceUsd).toFixed(2)}`;
+        newLi.innerText = `${name} (${symbol}): ${convertedBRL.toFixed(2)}`;
         CoinsContainer.appendChild(newLi);
     });
 }
