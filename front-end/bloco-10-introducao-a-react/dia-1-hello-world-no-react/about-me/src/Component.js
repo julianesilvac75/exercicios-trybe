3af181cb737dbf76eabb83b83fd7cc82cb26c66d
Juliane Cardoso <julianesilvac75@gmail.com>
2021-10-27 20:38:41 -0300

    feat: about-me - exercicio de fixacao

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/about-me/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class AboutMe extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Juliane Cardoso</h1>
+                <p>Estudante de desenvolvimento web, bissexual, autista, TDAH e AH/SD.</p>
+            </div>
+        )
+    }
+}
+
+export default AboutMe;
