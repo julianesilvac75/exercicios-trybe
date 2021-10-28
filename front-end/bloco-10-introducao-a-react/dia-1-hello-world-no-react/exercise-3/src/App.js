c0794fa678169c631ca3420416d52389a7248800
Juliane Cardoso <julianesilvac75@gmail.com>
2021-10-27 21:44:14 -0300

    feat: cria a classe Footer e renderiza na pagina

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise-3/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise-3/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Content from './Content';
+import Footer from './Footer';
 
 function App() {
   return (
     <>
       <header><Header/></header>
       <main><Content/></main>
+      <footer><Footer/></footer>
     </>
     );
 }
