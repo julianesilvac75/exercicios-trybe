aa112935f0d405d10988d777974180ff408d985b
Juliane Cardoso <julianesilvac75@gmail.com>
2021-11-04 16:11:42 -0300

    feat: exercicio de fixacao 6 concluido

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/fixacao-formularios/src/Form.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/fixacao-formularios/src/Form.js
@@ -28,72 +28,78 @@ class Form extends Component {
 
   render() {
     return (
-      <form>
+        <form>
 
-        <label>
-          Nome:
-          <input
-            type="text"
-            name="nome"
-            value={this.state.nome}
-            onChange={this.handleChange}
-            required
-          />  
-        </label>
+            <fieldset>
+                <legend>Dados pessoais</legend>
+                <label>
+                    Nome:
+                    <input
+                        type="text"
+                        name="nome"
+                        value={this.state.nome}
+                        onChange={this.handleChange}
+                        required
+                    />  
+                </label>
 
-        <label>
-          Idade:
-          <input
-            type="number"
-            name="idade"
-            value={this.state.idade}
-            onChange={this.handleChange}
-            required
-          />
-        </label>
+                <label>
+                    Idade:
+                    <input
+                        type="number"
+                        name="idade"
+                        value={this.state.idade}
+                        onChange={this.handleChange}
+                        required
+                    />
+                </label>
 
-        <label>
-          Estado:
-          <select
-            name="estado"
-            value={this.state.estado}
-            onChange={this.handleChange}
-            required
-          >
-            <option value="" selected disabled>-- Escolha um estado --</option>
-            <option>Paraná</option>
-            <option>Rio de Janeiro</option>
-            <option>São Paulo</option>
-            <option>Santa Catarina</option>
-            <option>Pará</option>
-          </select>
-        </label>
+                <label>
+                    Estado:
+                    <select
+                        name="estado"
+                        value={this.state.estado}
+                        onChange={this.handleChange}
+                        required
+                    >
+                        <option value="" defaultValue disabled>-- Escolha um estado --</option>
+                        <option>Paraná</option>
+                        <option>Rio de Janeiro</option>
+                        <option>São Paulo</option>
+                        <option>Santa Catarina</option>
+                        <option>Pará</option>
+                    </select>
+                </label>
+            </fieldset>
 
-        <label>
-          Sobre você:
-          <textarea
-            name="sobreVoce"
-            value={this.state.sobreVoce}
-            onChange={this.handleChange}
-            required
-          />
-        </label>
+            <fieldset>
+                <legend>Perfil</legend>
+                <label>
+                    Sobre você:
+                    <textarea
+                        name="sobreVoce"
+                        value={this.state.sobreVoce}
+                        onChange={this.handleChange}
+                        required
+                    />
+                </label>
 
-        <label>
-          Foto de perfil:
-          <input type="file" />
-        </label>
+                <label>
+                    Foto de perfil:
+                    <input type="file" />
+                    </label>
+            </fieldset>
 
-        <label>
-            <input
-              type="checkbox"
-              name="concordo"
-              onChange={this.handleChange}
-            />
-            Você concorda?
-        </label>
+            <label>
+                <input
+                type="checkbox"
+                name="concordo"
+                onChange={this.handleChange}
+                />
+                Você concorda?
+            </label>
 
-      </form>
+        </form>
       )
     }
 }
