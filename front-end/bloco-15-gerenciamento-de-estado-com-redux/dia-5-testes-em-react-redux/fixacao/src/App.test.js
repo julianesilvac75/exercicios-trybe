e94aa54c729dcb64250dc4aea28e6d137980f23b
Juliane Cardoso <julianesilvac75@gmail.com>
2022-01-07 20:21:57 -0300

    feat: exercicios de fixacao finalizados

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-5-testes-em-react-redux/fixacao/src/App.test.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-5-testes-em-react-redux/fixacao/src/App.test.js
@@ -5,6 +5,7 @@ import App from './App';
 import { createStore, combineReducers } from 'redux';
 import clickReducer from './reducers';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 const renderWithRedux = (
   component,
@@ -32,5 +33,30 @@ describe('testing clicks', () => {
     renderWithRedux(<App />, { INITIAL_STATE: { clickReducer: { counter: 5 }}});
 
     expect(screen.getByText('5')).toBeInTheDocument();
+  });
+
+  test('exercício de fixação 1: testa se um clique funciona', () => {
+    renderWithRedux(<App/>);
+    
+    expect(screen.getByText('0')).toBeInTheDocument();
+
+    userEvent.click(screen.getByRole('button', { name: /clique aqui/i }));
+  
+    expect(screen.queryByText('0')).not.toBeInTheDocument();
+    expect(screen.getByText('1')).toBeInTheDocument();
+  });
+
+  test('exercício de fixação 2: testa se um clique funciona com um valor inicial', () => {
+    renderWithRedux(
+      <App />,
+      { INITIAL_STATE: { clickReducer: { counter: 10}}}
+    );
+    expect(screen.getByText('10')).toBeInTheDocument();
+    expect(screen.queryByText('0')).not.toBeInTheDocument();
+
+    userEvent.click(screen.getByRole('button', { name: /clique aqui/i }));
+
+    expect(screen.queryByText('10')).not.toBeInTheDocument();
+    expect(screen.getByText('11')).toBeInTheDocument();
   });
 });
