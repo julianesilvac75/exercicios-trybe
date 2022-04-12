14797ffca3055426d6be2be742fdb54b58614eb9
Juliane Cardoso <julianesilvac75@gmail.com>
2022-04-12 20:04:32 -0300

    fixation exercises: eager loading

--- back-end/bloco-24-nodejs-orm-e-autenticacao/dia-2-orm-associations/fixation-exercises/seeders/20220412195045-addresses.js
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-2-orm-associations/fixation-exercises/seeders/20220412195045-addresses.js
@@ -12,6 +12,12 @@ module.exports = {
         },
         {
           city: 'São Paulo',
+          street: 'Avenida Paulista',
+          number: 1980,
+          employee_id: 2,
+        },
+        {
+          city: 'Fortaleza',
           street: 'Rua das Enseadas',
           number: 95,
           employee_id: 3,
@@ -23,10 +29,16 @@ module.exports = {
           employee_id: 4,
         },
         {
+          city: 'Belo Horizonte',
+          street: 'Rua Vicente Alvarenga',
+          number: 80,
+          employee_id: 1,
+        },
+        {
           city: 'Curitiba',
           street: 'Rua Fria',
           number: 101,
-          employee_id: 4,
+          employee_id: 5,
         },
       ],
       {},
