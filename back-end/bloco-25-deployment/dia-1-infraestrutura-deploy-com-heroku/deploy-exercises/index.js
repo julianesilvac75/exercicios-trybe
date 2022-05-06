207ba7f865ea68abe862f41388ad37ae79b2e690
Juliane Cardoso <julianesilvac75@gmail.com>
2022-05-06 18:44:12 -0300

    fixation exercises

--- back-end/bloco-25-deployment/dia-1-infraestrutura-deploy-com-heroku/deploy-exercises/index.js
+++ back-end/bloco-25-deployment/dia-1-infraestrutura-deploy-com-heroku/deploy-exercises/index.js
@@ -4,8 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const {SET_ENV} = process.env || 'Algo deu errado :(';
+
 app.get('/', (req, res) => {
-  res.send('Está vivo!!!');
+  res.send(`Está vivo!!! Essa é a variável do exercício 2: ${SET_ENV}`);
 });
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
